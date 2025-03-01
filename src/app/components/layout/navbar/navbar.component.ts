43248ca73be7eb630b0c38155226c20bb67d515a
OussamaBL <oussamaazrour@gmail.com>
2025-03-01 17:15:45 +0000

    configuration de tailwind

---
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterModule],
+  standalone:true,
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}" // Ensures Tailwind scans all components and templates
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
