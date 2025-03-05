eb54e9855c17e56ee7564e7bcd380f8dee6c8331
OussamaBL <oussamaazrour@gmail.com>
2025-03-05 12:46:36 +0000

    Integration de layouts nav et footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
+export const routes: Routes = [
+  { path: '', component: SignupComponent ,
+    /*canActivate: [redirectIfAuthenticateGuard] */
+  },
+  { path: 'register', component: SignupComponent ,
+    /*canActivate: [redirectIfAuthenticateGuard] */
+  },
+  { path: 'login', component: LoginComponent ,
+    /*canActivate: [redirectIfAuthenticateGuard] */
+  },
+  { path: 'forgetPassword', component: ForgetPasswordComponent ,
+    /*canActivate: [redirectIfAuthenticateGuard] */
+  },
 
-export const routes: Routes = [];
+];
--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import {NgIf} from "@angular/common";
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
+  imports: [RouterModule, NgIf],
   standalone:true,
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  menuOpen: boolean = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 
 }
